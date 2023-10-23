ced2d81f79fe0938d7abcdba7b81e23d5f51a4aa
alb202 <ablumenf@gmail.com>
2023-10-22 20:24:12 -0400

    Simplified HA function

--- src/ha.py
+++ src/ha.py
@@ -1,12 +1,45 @@
 from pandas import DataFrame
 from numpy import where
+from ta_utils import validate_columns
+
+
+# def heikin_ashi(df: pls.DataFrame) -> pls.DataFrame:
+#     """Heikin Ashi Algorithm"""
+
+#     df = pls.from_pandas(df)
+
+#     cols = ["symbol", "Open", "High", "Low", "Close", "Volume"]
+
+#     df = df.sort("Date").select(["Date"] + cols)  # .reset_index(drop=True)
+
+#     df = df.with_columns(
+#         pls.col("HA_Close"), df.select(["Open", "High", "Low", "Close"]).apply(lambda cols: sum(cols) / 4)
+#     )
+
+#     df["HA_Open"] = pls.Series.from_vec([float(0)] * len(df))
+
+#     df["HA_Open"].set(0, df["Open"].get(0))
+
+#     for index in range(1, len(df)):
+#         df["HA_Open"].set(index, (df["HA_Open"].get(index - 1) + df["HA_Close"].get(index - 1)) / 2)
+
+#     df["HA_High"] = df.select(["HA_Open", "HA_Close", "Low", "High"]).apply(lambda cols: cols.max())
+
+#     df["HA_Low"] = df.select(["HA_Open", "HA_Close", "Low", "High"]).apply(lambda cols: cols.min())
+
+#     df = df.rename("Heiken_Ashi")
+
+#     return df.select(["Date", "symbol", "HA_Open", "HA_High", "HA_Low", "HA_Close", "Volume"])
 
 
 def heikin_ashi(df: DataFrame) -> DataFrame:
     """Heikin Ashi Algorithm"""
-    cols = ["symbol", "Open", "High", "Low", "Close", "Volume"]
-    date_col = "Date"
-    df = df.sort_values(date_col, ascending=True).loc[:, [date_col] + cols].reset_index(drop=True)
+
+    cols = ["symbol", "Date", "Open", "High", "Low", "Close", "Volume"]
+
+    validate_columns(df_columns=df.columns, required_columns=cols)
+
+    df = df.sort_values("Date", ascending=True).loc[:, ["Date"] + cols].reset_index(drop=True)
 
     df["HA_Close"] = df.loc[:, ["Open", "High", "Low", "Close"]].apply(sum, axis=1).divide(4)
 
@@ -23,6 +56,9 @@ def heikin_ashi(df: DataFrame) -> DataFrame:
 
 def heikin_ashi_signals(df: DataFrame) -> DataFrame:
     """Heiken Ashi Signals"""
+
+    # df = pls.DataFrame(df)
+
     is_increasing = df["HA_Close"] > df["HA_Open"]
     is_increasing_yday = df["HA_Close"].shift(1) > df["HA_Open"].shift(1)
     buy_signal = (is_increasing & ~is_increasing_yday).replace({True: 1, False: 0})
@@ -37,3 +73,17 @@ def heikin_ashi_signals(df: DataFrame) -> DataFrame:
         ["Date", "symbol", "HA_Open", "HA_High", "HA_Low", "HA_Close", "Volume", "HA_Signal", "HA_Trend", "HA_Streak"],
     ]
 
+
+# from pandas import read_parquet
+# from pathlib import Path
+
+# data_path = Path("/Users/ab/Data/stock_data/").absolute()
+
+# df_ = read_parquet(data_path / "OHLCV/D/data.parquet")
+
+# print(df_)
+
+# # ha = heikin_ashi(df=df_.filter(pls.col("symbol") == "MSFT")).to_)
+# ha = heikin_ashi(df=df_.query("symbol == 'MSFT'"))
+
+# print(ha)
